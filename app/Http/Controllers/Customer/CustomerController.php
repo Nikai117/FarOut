aded74dc8952ff401652dcbbb2ba39f371f2f8e7
Nikai117 <97072138@st.deltion.nl>
2022-09-05 21:05:50 +0200

    ADD: Customer register function

---
+++ app/Http/Controllers/Customer/CustomerController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Customer;
+
+use App\Models\Customer;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CustomerController
+{
+    public function register(Request $request){
+
+        $customer = Customer::create([
+            'first_name' => $request->input('first_name'),
+            'last_name' => $request->input('last_name'),
+            'email' => $request->input('email'),
+            'address' => $request->input('address'),
+            'postal_code' => $request->input('postal_code'),
+            'city' => $request->input('city'),
+            'country_code' => $request->input('country_code'),
+            'phone' => $request->input('phone'),
+        ]);
+
+        return new JsonResource(
+            $customer
+        );
+
+    }
+}
