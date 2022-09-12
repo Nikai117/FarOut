27b2e189854dc8ed198cf6fb5e4c8c6c57688472
rozhan Arya <rozhanarya1@gmail.com>
2022-09-12 11:30:31 +0200

    ADD tekst

--- farout-react/src/Pages/CustomerForm.js
+++ farout-react/src/Pages/CustomerForm.js
@@ -5,6 +5,22 @@ function CustomerForm() {
     <div class='d-flex'>    
     <form>
     <div class="mb-3">
+      <label for="exampleInputname" class="form-label d-flex">Name</label>
+      <input type="text" class="form-control" id="exampleInputname"/>
+    </div>
+    <div class="mb-3">
+      <label for="exampleInputLname" class="form-label d-flex">Last name</label>
+      <input type="text" class="form-control" id="exampleInputLname"  />
+    </div>
+    <div class="mb-3">
+      <label for="exampleInputBdate" class="form-label d-flex">Date of birth</label>
+      <input type="date" class="form-control" id="exampleInputBdate" />
+    </div>
+    <div class="mb-3">
+      <label for="exampleInputPhone" class="form-label d-flex">Phone number</label>
+      <input type="text" class="form-control" id="exampleInputPhone" />
+    </div>
+    <div class="mb-3">
       <label for="exampleInputEmail1" class="form-label d-flex">Email address</label>
       <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
     </div>
