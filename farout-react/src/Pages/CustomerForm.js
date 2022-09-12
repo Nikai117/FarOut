03364854f6bb846095e0d1a1d7a92f6b3b3bac61
Lucaspinder <97078466@st.deltion.nl>
2022-09-12 14:44:52 +0200

    form to middle of the screen

--- farout-react/src/Pages/CustomerForm.js
+++ farout-react/src/Pages/CustomerForm.js
@@ -2,33 +2,33 @@ import React from 'react';
 
 function CustomerForm() {
   return (
-    <div class='d-flex'>    
+    <div className='d-flex justify-content-center'>    
     <form>
-    <div class="mb-3">
-      <label for="exampleInputname" class="form-label d-flex">Name</label>
-      <input type="text" class="form-control" id="exampleInputname"/>
+    <div className="mb-3">
+      <label htmlFor="exampleInputname" className="form-label d-flex">Name</label>
+      <input type="text" className="form-control" id="exampleInputname"/>
     </div>
-    <div class="mb-3">
-      <label for="exampleInputLname" class="form-label d-flex">Last name</label>
-      <input type="text" class="form-control" id="exampleInputLname"  />
+    <div className="mb-3">
+      <label htmlFor="exampleInputLname" className="form-label d-flex">Last name</label>
+      <input type="text" className="form-control" id="exampleInputLname"  />
     </div>
-    <div class="mb-3">
-      <label for="exampleInputBdate" class="form-label d-flex">Date of birth</label>
-      <input type="date" class="form-control" id="exampleInputBdate" />
+    <div className="mb-3">
+      <label htmlFor="exampleInputBdate" className="form-label d-flex">Date of birth</label>
+      <input type="date" className="form-control" id="exampleInputBdate" />
     </div>
-    <div class="mb-3">
-      <label for="exampleInputPhone" class="form-label d-flex">Phone number</label>
-      <input type="text" class="form-control" id="exampleInputPhone" />
+    <div className="mb-3">
+      <label htmlFor="exampleInputPhone" className="form-label d-flex">Phone number</label>
+      <input type="text" className="form-control" id="exampleInputPhone" />
     </div>
-    <div class="mb-3">
-      <label for="exampleInputEmail1" class="form-label d-flex">Email address</label>
-      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+    <div className="mb-3">
+      <label htmlFor="exampleInputEmail1" className="form-label d-flex">Email address</label>
+      <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
     </div>
-    <div class="mb-3">
-      <label for="exampleInputPassword1" class="form-label d-flex">Password</label>
-      <input type="password" class="form-control" id="exampleInputPassword1" />
+    <div className="mb-3">
+      <label htmlFor="exampleInputPassword1" className="form-label d-flex">Password</label>
+      <input type="password" className="form-control" id="exampleInputPassword1" />
     </div>
-    <button type="submit" class="btn btn-primary d-flex">Submit</button>
+    <button type="submit" className="btn btn-primary d-flex">Submit</button>
   </form>
   </div>
   )
