698f1834d9e1553e1fecd847c41496038bc4c535
Lucaspinder <97078466@st.deltion.nl>
2022-09-12 11:03:56 +0200

    Formulier fixes

--- farout-react/src/Pages/CustomerForm.js
+++ farout-react/src/Pages/CustomerForm.js
@@ -2,19 +2,21 @@ import React from 'react';
 
 function CustomerForm() {
   return (
+    <div class='d-flex'>    
     <form>
-      <div class="form-group">
-        <label for="exampleInputEmail1">Email address</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
-        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-      </div>
-      <div class="form-group">
-        <label for="exampleInputPassword1">Password</label>
-        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" />
-      </div>
-      <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
+    <div class="mb-3">
+      <label for="exampleInputEmail1" class="form-label d-flex">Email address</label>
+      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+    </div>
+    <div class="mb-3">
+      <label for="exampleInputPassword1" class="form-label d-flex">Password</label>
+      <input type="password" class="form-control" id="exampleInputPassword1" />
+    </div>
+    <button type="submit" class="btn btn-primary d-flex">Submit</button>
+  </form>
+  </div>
   )
+
 }
 
 export default CustomerForm
