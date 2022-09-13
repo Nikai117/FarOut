57d9ff286c8c30b5c34850c5d160e1dcf80cedc1
Nikai117 <97072138@st.deltion.nl>
2022-09-13 13:50:12 +0200

    ADD: Countries Collection

--- app/Http/Controllers/Country/CountryController.php
+++ app/Http/Controllers/Country/CountryController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Country;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Country\CountryCollection;
+use App\Http\Resources\Country\CountryResource;
 use App\Models\Country;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
@@ -11,7 +13,7 @@ class CountryController
 {
     public function index(Country $country) {
 
-        return new JsonResource(
+        return new CountryCollection(
           Country::all()
         );
     }
--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Customer\CustomerController;
 use App\Http\Controllers\Reservation\ReservationController;
 use App\Http\Controllers\City\CityController;
 use App\Http\Controllers\Trip\TripController;
+use App\Http\Controllers\Country\CountryController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
@@ -16,3 +17,4 @@ Route::resource('/customer/{customer}/reservations', ReservationController::clas
 Route::resource('/customer', CustomerController::class)->only(['show', 'index']);
 Route::resource('/city', CityController::class)->only(['index']);
 Route::resource('/city/{city}/trips', TripController::class)->only(['index']);
+Route::resource('/countries', CountryController::class)->only(['index']);
