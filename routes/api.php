a37cf4bb957c4dd1d92c21e04e0c93f81d66153d
Mychon <97082598@st.deltion.nl>
2022-09-13 13:27:06 +0200

    Nieuwe API routes

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Customer\CustomerController;
-use \App\Http\Controllers\Reservation\ReservationController;
+use App\Http\Controllers\Reservation\ReservationController;
+use App\Http\Controllers\City\CityController;
+use App\Http\Controllers\Trip\TripController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
@@ -12,3 +14,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::post('/register', [CustomerController::class, 'register']);
 Route::resource('/customer/{customer}/reservations', ReservationController::class)->only(['index']);
 Route::resource('/customer', CustomerController::class)->only(['show', 'index']);
+Route::resource('/city', CityController::class)->only(['index']);
+Route::resource('/city/{city}/trips', TripController::class)->only(['index']);
