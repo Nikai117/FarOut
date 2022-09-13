1352d72d0dcd4c79574596124ba8dedfea369369
Nikai117 <97072138@st.deltion.nl>
2022-09-13 13:56:05 +0200

    ADD Collection and relations country

--- app/Http/Controllers/Country/CountryController.php
+++ app/Http/Controllers/Country/CountryController.php
@@ -2,12 +2,8 @@
 
 namespace App\Http\Controllers\Country;
 
-use App\Http\Controllers\Controller;
 use App\Http\Resources\Country\CountryCollection;
-use App\Http\Resources\Country\CountryResource;
 use App\Models\Country;
-use Illuminate\Http\Request;
-use Illuminate\Http\Resources\Json\JsonResource;
 
 class CountryController
 {
--- app/Models/City.php
+++ app/Models/City.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -20,5 +21,10 @@ class City extends Model
     public function trips(): HasMany
     {
         return $this->hasMany(Trip::class);
+    }
+
+    public function country(): BelongsTo
+    {
+        return $this->belongsTo(Country::class);
     }
 }
