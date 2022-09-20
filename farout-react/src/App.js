e6b8758f99f8cb4db2dbb22ea0af6ada8ad13d17
Lucaspinder <97078466@st.deltion.nl>
2022-09-20 13:01:27 +0200

    Customerform done

--- farout-react/src/App.js
+++ farout-react/src/App.js
@@ -1,44 +1,62 @@
-import './App.css';
-import AppRouter from './AppRouter';
+import "./App.css";
+import AppRouter from "./AppRouter";
 //import Footer from '../src/components/MainSection/index';
-
 
 export default function App() {
   return (
     <div className="App">
-    <nav className="navbar navbar-expand-lg  navbar-dark bg-dark">
-      <a className="navbar-brand" href="/">Farout</a>
-      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNavDropdown">
-        <ul className="navbar-nav ">
-          <li className="nav-item active">
-          <a className="nav-link" href="/">Home</a>
-          </li>
-          <li className="nav-item">
-          <a className="nav-link" href="http://localhost:3000/allcustomers">All Customers</a>
-          </li>
-          <li className="nav-item">
-          <a className="nav-link" href="http://localhost:3000/customerform">Customerform</a>
-          </li>
-        </ul>
-        <ul className="navbar-nav ms-auto">
-        <li className="nav-item">
-            <a className='nav-link' href="http://localhost:3000/login">Login</a>
-        </li>
-        </ul>
+      <nav className="navbar navbar-expand-lg  navbar-dark bg-dark">
+        <a className="navbar-brand" href="/">
+          Farout
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavDropdown"
+          aria-controls="navbarNavDropdown"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul className="navbar-nav ">
+            <li className="nav-item active">
+              <a className="nav-link" href="/">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="http://localhost:3000/allcustomers">
+                All Customers
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="http://localhost:3000/customerform">
+                Customerform
+              </a>
+            </li>
+          </ul>
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <a className="nav-link" href="http://localhost:3000/login">
+                Login
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <AppRouter />
+      <div id="footer">
+        <footer className="bg-dark text-center text-lg-start">
+          <div className="text-center p-3">
+            <a className="text-light" href="/">
+              Farout
+            </a>
+          </div>
+        </footer>
       </div>
-    </nav>
-    <AppRouter />
-    <div id="footer">    
-    <footer className="bg-dark text-center text-lg-start">
-      <div className="text-center p-3">
-        <a className="text-light" href="/">Farout</a>
-      </div>
-    </footer>
     </div>
-    </div>
-
   );
 }
